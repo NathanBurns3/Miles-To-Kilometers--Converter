ae8515bff8d9925c78c6e473ecc2c651f4fe0386
Nathan Burns <91291671+NathanBurns3@users.noreply.github.com>
2023-01-20 14:41:26 -0500

    Update index.js
    added comments

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+//constant conversion variable
 const conversion = 1.60934;
 
+//miles variable
 let miles = 20;
 
+//prints the conversion calculation
 console.log("The distance of " + (miles * conversion) + " kms is equal to " + miles + " miles");
